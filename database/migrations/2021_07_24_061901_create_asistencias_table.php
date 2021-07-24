c6651dc699ba2816229ca0b9af770027d8146e47
Luis Benitez <lfbenitez15@gmail.com>
2021-07-24 15:40:09 -0600

    [Add] Model,Migration, Factory, Controller, Seeder para asistencias
    con el comando php artisan make:model modelo -a
    
    Se agregaron los atributos correspondientes al  migración

---
+++ database/migrations/2021_07_24_061901_create_asistencias_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAsistenciasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asistencias', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->time('entrada', 0);
+            $table->time('salida', 0)->nullable();
+            $table->unsignedBigInteger('empleado_id');
+            $table->foreign('empleado_id')->references('id')->on('empleados')
+            ->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asistencias');
+    }
+}
