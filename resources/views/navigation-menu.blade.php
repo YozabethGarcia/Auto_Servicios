e378ef2be38eeccc37392c44b2a32541084bf4f9
hll3gt14 <german.tejada@unah.hn>
2021-08-04 13:28:34 -0600

    fix

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,12 +16,6 @@
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('ingresar') }}" :active="request()->routeIs('ingresar')">
-                        {{ __('Ingresar Producto') }}
-                    </x-jet-nav-link>
-                </div>
-            </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
@@ -146,12 +140,6 @@
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
-        </div>
-
-        <div class="pt-2 pb-3 space-y-1">
-            <x-jet-nav-link href="{{ route('ingresar') }}" :active="request()->routeIs('ingresar')">
-                {{ __('Ingresar Producto') }}
-            </x-jet-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,3 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/ingresar', function () {
-    return view('ingresar');
-})->name('ingresar');
